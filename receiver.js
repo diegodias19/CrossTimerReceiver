06460b0be15fd7e3f4f11bcb5ee4888108ccbdef
diegodias19 <diegodias19@gmail.com>
2026-07-27 21:45:16 -0300

    Implement custom message listener in receiver.js
    Added custom message listener for Android messages.

--- receiver.js
+++ receiver.js
@@ -1,5 +1,16 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 
-const options = new cast.framework.CastReceiverOptions();
+// Definimos o mesmo namespace que usaremos no Android
+const CUSTOM_NAMESPACE = 'urn:x-cast:br.com.diego.crosstimer';
 
+// Escuta mensagens recebidas no nosso namespace
+context.addCustomMessageListener(CUSTOM_NAMESPACE, (event) => {
+    // event.data conterá a mensagem enviada pelo Android (ex: "10" ou JSON)
+    const contadorDiv = document.getElementById('contador');
+    if (contadorDiv) {
+        contadorDiv.textContent = event.data;
+    }
+});
+
+const options = new cast.framework.CastReceiverOptions();
 context.start(options);
